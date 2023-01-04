a7edd22876e66c871873389d2c34c9cc96a2b26c
ed-morris-LRe <113369845+ed-morris-LRe@users.noreply.github.com>
2023-01-04 17:24:56 +0000

    Update 05 - 🧭 Navigating the AnalyzeRe API.py

--- pages/05 - 🧭 Navigating the AnalyzeRe API.py
+++ pages/05 - 🧭 Navigating the AnalyzeRe API.py
@@ -10,11 +10,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from PIL import Image
 import streamlit as st
-import os
+#import os
 
 # set wd
-rfp_path = r"C:\Users\ed.morris\Documents\Python Scripts\Streamlit SAGE"
-os.chdir(rfp_path)
+#rfp_path = r"C:\Users\ed.morris\Documents\Python Scripts\Streamlit SAGE"
+#os.chdir(rfp_path)
 
 # Set page configuration
 st.set_page_config(page_title='Demo Upload - Actuarial UI',page_icon='🧭',initial_sidebar_state='expanded')
